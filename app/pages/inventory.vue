d6b5533b99e2d421fcca83f04cf7cd50e8c489cf
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-09-16 21:19:08 +0200

    feat: auth

--- app/pages/inventory.vue
+++ app/pages/inventory.vue
@@ -1,3 +1,12 @@
+<script setup>
+const supabase = useSupabaseClient()
+</script>
 <template>
   inventory
+
+  <button
+    class="mt-3"
+    @click="supabase.auth.signOut()">
+    Log out
+  </button>
   </template>
