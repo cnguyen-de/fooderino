ec999e8a44f1808ae0849af2e3d3bba9dc442edd
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-10-04 17:10:40 +0200

    feat: categories/markets in list

--- app/pages/buy.vue
+++ app/pages/buy.vue
@@ -1,10 +1,18 @@
 <script setup>
 const { data } = await useAsyncData('items', async () => {
   return await fetchBuyItems();
-})
+});
 const colorMode = useColorMode();
 colorMode.preference = 'dark';
+
+const categories = computed(() => {
+  const stores = data.value.map((item) => item.store);
+  return [...new Set(stores)];
+});
 </script>
 <template>
-  <BuyList :items="data"></BuyList>
+  <div v-for="category in categories" :key="category">
+    <h2 class="px-2 font-bold text-white">{{ category }}</h2>
+    <BuyList :items="data.filter((item) => item.store === category)"></BuyList>
+  </div>
 </template>
--- app/pages/inventory.vue
+++ app/pages/inventory.vue
@@ -4,11 +4,14 @@ const supabase = useSupabaseClient();
 const { data } = await useAsyncData('items', async () => {
   return await fetchInventoryItems();
 });
-const categories = computed(() => data.value.map((item) => item.location));
+const categories = computed(() => {
+  const cat = data.value.map((item) => item.location);
+  return [...new Set(cat)];
+});
 </script>
 <template>
   <div v-for="category in categories" :key="category">
-    <h2 class="px-2 text-white font-bold">{{ category }}</h2>
+    <h2 class="px-2 font-bold text-white">{{ category }}</h2>
     <InventoryList :items="data.filter((item) => item.location === category)"></InventoryList>
   </div>
 </template>
