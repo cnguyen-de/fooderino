2e33f7f260a1cf726b5cbd9184ba85568e4a6556
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-11-05 14:28:46 +0100

    feat: add avatar

--- app/components/footer/Footer.vue
+++ app/components/footer/Footer.vue
@@ -1,4 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useSupabaseUser } from '#imports';
+
+const user = useSupabaseUser();
+const usernameFallback = computed(() =>
+  user.value.user_metadata?.full_name
+    .split(' ')
+    .map((n) => n[0])
+    .join('')
+);
+console.log(user);
+</script>
 
 <template>
   <div class="flex h-full flex-row items-center justify-between border-t border-solid border-gray-500/50 p-2">
@@ -32,18 +43,10 @@
     </FooterLink>
     <FooterLink link="/ai">🪄</FooterLink>
     <FooterLink link="/settings">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="size-6">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M4.5 12a7.5 7.5 0 0 0 15 0m-15 0a7.5 7.5 0 1 1 15 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077 1.41-.513m14.095-5.13 1.41-.513M5.106 17.785l1.15-.964m11.49-9.642 1.149-.964M7.501 19.795l.75-1.3m7.5-12.99.75-1.3m-6.063 16.658.26-1.477m2.605-14.772.26-1.477m0 17.726-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205 12 12m6.894 5.785-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" />
-      </svg>
+      <Avatar referrerpolicy="no-referrer">
+        <AvatarImage :src="user?.user_metadata.picture" alt="Avatar" referrerpolicy="no-referrer" />
+        <AvatarFallback>{{ usernameFallback }}</AvatarFallback>
+      </Avatar>
     </FooterLink>
   </div>
 </template>
