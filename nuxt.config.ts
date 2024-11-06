59a564c70e891a085af5c75ca35c7d9c4fe9b543
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-11-06 14:16:38 +0100

    feat: pwa manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,5 +44,73 @@ export default defineNuxtConfig({
     optimizeDeps: {
       exclude: ['vee-validate']
     }
+  },
+  pwa: {
+    manifest: {
+      name: 'Fooderino',
+      short_name: 'Fooderino',
+      description: 'Fooderino is a check list app for groceries.',
+      theme_color: '#000000',
+      lang: 'en',
+      icons: [
+        {
+          src: '/android-icon-36x36.png',
+          sizes: '36x36',
+          type: 'image/png',
+          density: '0.75'
+        },
+        {
+          src: '/android-icon-48x48.png',
+          sizes: '48x48',
+          type: 'image/png',
+          density: '1.0'
+        },
+        {
+          src: '/android-icon-72x72.png',
+          sizes: '72x72',
+          type: 'image/png',
+          density: '1.5'
+        },
+        {
+          src: '/android-icon-96x96.png',
+          sizes: '96x96',
+          type: 'image/png',
+          density: '2.0'
+        },
+        {
+          src: '/android-icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+          density: '3.0'
+        },
+        {
+          src: '/android-icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          density: '4.0'
+        }
+      ],
+      screenshots: [
+        {
+          src: '/screenshot-handset.png',
+          sizes: '1082x2402',
+          type: 'image/png',
+          form_factor: 'narrow'
+        },
+        {
+          src: '/screenshot-wide.png',
+          sizes: '3840x2160',
+          type: 'image/png',
+          form_factor: 'wide'
+        }
+      ]
+    },
+    workbox: {
+      navigateFallback: '/'
+    },
+    devOptions: {
+      enabled: true,
+      type: 'module'
+    }
   }
 });
