d657d959fb476764ff3eae9befee0090e4c33ab4
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-11-06 13:16:59 +0100

    feat: add tracking

--- server/plugins/extend-html.ts
+++ server/plugins/extend-html.ts
@@ -2,6 +2,9 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('render:html', (html, { event }) => {
     // This will be an object representation of the html template.
     html.head.push(`<link rel="manifest" href="/manifest.webmanifest">`);
+    html.head.push(
+      `<script defer src="https://cloud.umami.is/script.js" data-website-id="33fa216d-5188-4249-b6ea-391e4aae906d"></script>`
+    );
   });
   // You can also intercept the response here.
   nitroApp.hooks.hook('render:response', (response, { event }) => {});
