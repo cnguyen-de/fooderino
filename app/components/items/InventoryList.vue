307ebe154130e47df412ffb7fc0cfe1cdf83d186
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-11-19 16:09:17 +0100

    feat: Add list transition

--- app/components/items/InventoryList.vue
+++ app/components/items/InventoryList.vue
@@ -19,20 +19,35 @@ const onItemAmountValueChanged = (item: Item) => {
 
 <template>
   <div class="flex flex-col justify-center gap-1 p-4">
-    <Item
-      v-for="item in items"
-      :key="item.id"
-      :id="item.id"
-      :name="item.name"
-      :amount="item.amount"
-      :amount_type="item.amount_type"
-      :amount_to_purchase="item.amount_to_purchase"
-      :to-right="true"
-      :show-amount="false"
-      :disable-swipe="true"
-      @value-changed="onItemAmountValueChanged($event)">
-      <span class="tabular-nums text-gray-400/50">{{ item.amount_to_purchase }}</span>
-      <AddToBuy :item="item"></AddToBuy>
-    </Item>
+    <TransitionGroup>
+      <Item
+        v-for="item in items"
+        :key="item.id"
+        :id="item.id"
+        :name="item.name"
+        :amount="item.amount"
+        :amount_type="item.amount_type"
+        :amount_to_purchase="item.amount_to_purchase"
+        :to-right="true"
+        :show-amount="false"
+        :disable-swipe="true"
+        @value-changed="onItemAmountValueChanged($event)">
+        <span class="tabular-nums text-gray-400/50">{{ item.amount_to_purchase }}</span>
+        <AddToBuy :item="item"></AddToBuy>
+      </Item>
+    </TransitionGroup>
   </div>
 </template>
+<style scoped>
+.v-enter-active {
+  transition: all 0.25s ease-in;
+}
+.v-leave-active {
+  transition: all 0.25s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
