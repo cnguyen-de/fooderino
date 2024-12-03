69e2a0433713255a8ecac6acabb75c8157d43634
Chi Nguyen <c.nguyen.prog@gmail.com>
2024-12-03 12:01:57 +0100

    feat: Update create new list button

--- app/components/lists/ListSelector.vue
+++ app/components/lists/ListSelector.vue
@@ -75,6 +75,7 @@ const onRemoveUser = async (email: string, isInvite = false) => {
         <DropdownMenuRadioGroup v-model="selectedDropDownItem">
           <DropdownMenuRadioItem v-for="list of lists" :value="list">{{ list?.name }}</DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
+        <DropdownMenuSeparator />
         <DropdownMenuItem>
           <button @click="openCreateListDrawer()" class="flex items-center justify-center gap-2">
             <svg
@@ -83,11 +84,8 @@ const onRemoveUser = async (email: string, isInvite = false) => {
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="size-6">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
+              class="size-4">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
             Create New List
           </button>
