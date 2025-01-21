97817bb23518333463a6bbd634e797850fdd9453
Chi Nguyen <c.nguyen.prog@gmail.com>
2025-01-21 11:50:17 +0100

    feat: Update search field icon colors

--- app/components/items/FilterItemsInput.vue
+++ app/components/items/FilterItemsInput.vue
@@ -11,7 +11,7 @@ watchDebounced(
   () => {
     itemStore.setFilterInput(input.value);
   },
-  { debounce: 300, maxWait: 500 }
+  { debounce: 500, maxWait: 800 }
 );
 
 const inputField = useTemplateRef('input-field');
@@ -23,19 +23,19 @@ const focusInputField = () => {
 <template>
   <div class="relative">
     <input
-      class="peer right-0 h-9 rounded-full !border-0  px-4 pr-7 text-gray-700 transition-all focus:w-full focus-visible:border-none focus-visible:outline-none focus-visible:ring-0  dark:text-gray-300 focus:bg-gray-100/30 focus:dark:bg-gray-700/30"
+      class="peer right-0 h-9 rounded-full !border-0  px-4 pr-7 text-gray-700 transition-all duration-150 ease-in-out focus:w-full focus-visible:border-none focus-visible:outline-none focus-visible:ring-0  dark:text-gray-300 focus:bg-gray-100/30 focus:dark:bg-gray-700/30"
       :class="[input.length !== 0 ? 'w-full bg-gray-100/30 dark:bg-gray-700/30' : 'w-10 bg-transparent']"
       placeholder="Find items"
       ref="input-field"
       v-model="input">
     </input>
     <button
-      class="absolute right-3 top-2 text-gray-700"
+      class="absolute right-3 top-2 text-gray-700 dark:text-gray-300"
       :class="[input.length !== 0 ? 'hidden' : 'block']"
       @click="focusInputField">
       <SearchIcon class="size-5"></SearchIcon>
     </button>
-    <button class="absolute right-3 top-2.5 text-gray-700" :class="[input.length === 0 ? 'hidden' : 'block']" @click="input = ''">
+    <button class="absolute right-3 top-2.5 text-gray-700 dark:text-gray-300" :class="[input.length === 0 ? 'hidden' : 'block']" @click="input = ''">
       <X class="size-4"></X>
     </button>
   </div>
